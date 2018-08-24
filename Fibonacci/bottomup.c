040b8c6f4fc7973b5bc5b8cb2ea7bab62a691df7
Rayner <raynersulyak@gmail.com>
2018-08-24 20:52:38 -0300

    added comments and personal touch

--- Fibonacci/bottomup.c
+++ Fibonacci/bottomup.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+
+int fib_bottom(int n);
+
 int main(){
 	int num;
-	scanf("%d",&num);
-	printf("%d",fib_bottom(num));
+	scanf("%d", &num);
+	printf("%d", fib_bottom(num));
 }
-int fib_bottom(int n){
-	int i,fib[n];
-	fib[1-1]=1;
-	fib[2-1]=1;
-	for(i=3-1;i<n;i++)
-		fib[i]=fib[i-1]+fib[i-2];
-	return fib[n-1];
+
+int fib_bottom(int n){ // retorna o n-esimo numero da sequencia fibonacci
+	int i, fib[n];
+	fib[0] = 1; // casos base
+	fib[1] = 1;	 
+	for(i = 2; i < n; i++) // do terceiro elemento ate o desejado
+		fib[i] = fib[i - 1] + fib[i - 2]; // fib(n) = fib(n - 1) + fib(n - 2)
+	return fib[n - 1];
 } 
