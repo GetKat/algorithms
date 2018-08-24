97ebc8bb313940ae2904650b6afcb00443070862
Rayner <raynersulyak@gmail.com>
2018-08-24 20:39:32 -0300

    added union find data struct

---
+++ UnionFind/unionfind.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+struct UnionFind{
+	vector<int> dad;
+
+	UionFind(int n){
+		dad.resize(n);
+
+		//iota(dad.begin(), dad.end(), 0);
+		for(int i = 0; i < n; i++)
+			dad[i] = i;
+	}
+
+	int finds(int u){ //funcao para a achar a raiz
+		if(dad[u] == u){ //ele eh seu propio pai
+			return u;
+		}
+		dad[u] = finds(dad[u]);
+		return dad[u];
+	}
+
+	bool unions(int u, int v){ //unir o conjunto v ao conjunto u
+		if(finds(u) == finds(v))
+			return false;
+		dad[dad[v]] = dad[u]; //agora compartilham a mesma raiz
+		return true;
+	}
+}
