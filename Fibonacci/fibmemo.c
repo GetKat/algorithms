36e5c10cababe79ca09c5597c15d9b166b849af5
Rayner <raynersulyak@gmail.com>
2018-08-24 21:26:05 -0300

    added a few comments

--- Fibonacci/fibmemo.c
+++ Fibonacci/fibmemo.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int fib_memo(int n, int *memo);
+
 int main(){
-	int num;
-	int *memo;
-	int i;
-//	scanf("%d",&num);
-	num=25;
-	memo=(int *)malloc((num+1)*sizeof(int));
-	for(i=0;i<=num;i++)
-		memo[i]=0;
-	printf("%d",fib_memo(num,memo));
+	int num, *memo;
+
+	scanf("%d", &num);
+	memo = (int *) malloc( num * sizeof(int)); //assigning num integers to memo
+	for(int i = 0; i < num; i++)
+	  	memo[i] = -1;
+	printf("%d", fib_memo(--num, memo)); //--num cause the array is 0-indexed
 }
-int fib_memo(int n,int *memo){
+
+int fib_memo(int n, int *memo){
 	int result;
-	if(memo[n]!=0)
-		return memo[n];
-	if(n==1||n==2)
-		result=1;
+
+	if(memo[n] != -1) //if I already calculated this fib number
+		return memo[n]; //he is stored and I shall not re-calculate him
+
+	if(n == 0|| n == 1) //base cases of fib recursion
+		result = 1;
 	else
-		result=fib_memo(n-1,memo)+fib_memo(n-2,memo);
-	memo[n]=result;
+		result = fib_memo(n-1, memo) + fib_memo(n-2, memo); //fib(n) = fib(n - 1) + fib(n - 2)
+	memo[n] = result; //now that I calculated it for the first time, I shall store it
 	return result;
 }
 
