a3530f5f74ba558c6af84dea1da89f29a323d4d2
Rayner <raynersulyak@gmail.com>
2018-08-24 21:30:58 -0300

    added some prefs

--- Fibonacci/fibmemo.c
+++ Fibonacci/fibmemo.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int fib_memo(int n, int *memo);
 
@@ -8,23 +7,18 @@ int main(){
 	int num, *memo;
 
 	scanf("%d", &num);
-	memo = (int *) malloc( num * sizeof(int)); //assigning num integers to memo
+	memo = (int *) malloc( num * sizeof(int)); // assigning num integers to memo
+
 	for(int i = 0; i < num; i++)
-	  	memo[i] = -1;
-	printf("%d", fib_memo(--num, memo)); //--num cause the array is 0-indexed
+	  	memo[i] = -1; // having a -1 value means that the fib number is not yet calculated
+
+	printf("%d", fib_memo(--num, memo)); // --num cause the array is 0-indexed
 }
 
 int fib_memo(int n, int *memo){
-	int result;
-
-	if(memo[n] != -1) //if I already calculated this fib number
-		return memo[n]; //he is stored and I shall not re-calculate him
-
-	if(n == 0|| n == 1) //base cases of fib recursion
-		result = 1;
-	else
-		result = fib_memo(n-1, memo) + fib_memo(n-2, memo); //fib(n) = fib(n - 1) + fib(n - 2)
-	memo[n] = result; //now that I calculated it for the first time, I shall store it
-	return result;
+	if(memo[n] != -1) // if I already calculated this fib number
+		return memo[n]; // he is stored and I shall not re-calculate him
+	return memo[n] = (!n || n == 1) ? 1 : fib_memo(n - 1, memo) + fib_memo(n - 2, memo);
+	// base cases (n == 0 and n == 1) else recursive formula fib(n) = fib(n - 1) + fib(n - 2)
 }
 
