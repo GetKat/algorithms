ffb505e892b486c40262da10a2af5c54e7f650ec
Rayner <raynersulyak@gmail.com>
2018-09-07 03:15:24 -0300

    added how to iterate a linked list

---
+++ lista encadead/iteration.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void show_content(void);
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+void show_content(void){
+    struct node *aux = head;
+
+    if(aux == (struct node *) NULL)
+        printf("Lista vazia!\n");
+    else
+        while(aux != (struct node *) NULL){
+            printf("%d ", aux->data);
+            aux = aux->next;
+        }
+    printf("\n");
+    
+    // Alternative iteration:
+    for(struct node *aux = head; aux != (struct node *) NULL; aux = aux->next)
+        printf("%d ", aux->data);
+    printf("\n");
+}
